00bdbe5110c0d3316115ca7963e6f76f8cf0affd
KarveJo <giorgio.zoppi@gmail.com>
2018-12-03 19:45:16 +0100

    new file:   constants/actions.js 	new file:   constants/netconfig.js

---
+++ src/react/constants/actions.js
@@ -0,0 +1,47 @@
+export const LOGIN_REQUEST = "LOGIN_REQUEST";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAILURE = "LOGIN_FAILURE";
+export const LOGOUT_REQUEST = "LOGOUT_REQUEST";
+export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
+export const LOGOUT_FAILURE = "LOGOUT_FAILURE";
+
+//---------------------------------------------------------------------
+
+export const LOAD_CONTRACTS_REQUEST = "LOAD_CUSTOMERS_REQUEST";
+export const LOAD_CONTRACTS_SUCCESS = "LOAD_CUSTOMERS_SUCCESS";
+export const LOAD_CONTRACTS_FAILURE = "LOAD_CUSTOMERS_FAILURE";
+
+export const GET_CONTRACT_REQUEST = "GET_CONTRACT_REQUEST";
+export const GET_CONTRACT_SUCCESS = "GET_CONTRACT_SUCCESS";
+export const GET_CONTRACT_FAILURE = "GET_CONTRACT_FAILURE";
+
+export const NEW_CONTRACT_REQUEST = "NEW_CONTRACT_REQUEST";
+
+export const UPDATE_CONTRACT_REQUEST = "UPDATE_CONTRACT_REQUEST";
+export const UPDATE_CONTRACT_SUCCESS = "UPDATE_CONTRACT_SUCCESS";
+export const UPDATE_CONTRACT_FAILURE = "UPDATE_CONTRACT_FAILURE";
+
+export const ADD_CUSTOMER_REQUEST = "ADD_CONTRACT_REQUEST";
+export const ADD_CUSTOMER_SUCCESS = "ADD_CONTRACT_SUCCESS";
+export const ADD_CUSTOMER_FAILURE = "ADD_CONTRACT_FAILURE";
+
+export const DELETE_CONTRACT_REQUEST = "DELETE_CONTRACT_REQUEST";
+export const DELETE_CONTRACT_SUCCESS = "DELETE_CONTRACT_SUCCESS";
+export const DELETE__FAILURE = "DELETE_CONTRACT_FAILURE";
+//---------------------------------------------------------------------
+export const LOAD_COUNTRY_REQUEST = "LOAD_COUNTRY_REQUEST";
+export const LOAD_COUNTRY_SUCCESS = "LOAD_COUNTRY_SUCCESS";
+export const LOAD_COUNTRY_FAILURE = "LOAD_COUNTRY_FAILURE";
+//---------------------------------------------------------------------
+export const LOAD_CITY_REQUEST = "LOAD_CITY_REQUEST";
+export const LOAD_CITY_SUCCESS = "LOAD_CITY_SUCCESS";
+export const LOAD_CITY_FAILURE = "LOAD_CITY_FAILURE";
+//---------------------------------------------------------------------
+export const LOAD_CITY_REQUEST = "LOAD_CITY_REQUEST";
+export const LOAD_CITY_SUCCESS = "LOAD_CITY_SUCCESS";
+export const LOAD_CITY_FAILURE = "LOAD_CITY_FAILURE";
+//---------------------------------------------------------------------
+export const LOAD_CITY_REQUEST = "LOAD_PROVINCE_REQUEST";
+export const LOAD_CITY_SUCCESS = "LOAD_PROVINCE_SUCCESS";
+export const LOAD_CITY_FAILURE = "LOAD_PROVINCE_FAILURE";
+
---
+++ src/react/constants/netconfig.js
@@ -0,0 +1,8 @@
+export default const NetworkConfig = {
+    "baseUri": "http://test.karveinformatica.com:14000",
+    "contractODataPath": "/odata/Contracts",
+    "contractApiPath":"/api/Contract",
+    "bookingODataPath": "/odata/Bookings",
+    "bookingApiPath": "/api/Booking"    
+}
+
