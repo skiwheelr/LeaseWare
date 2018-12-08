5c5ebb8c6b13ac1ec48d1ccac6ad62106982fb7d
skiwheelr <skiwheelr@gmail.com>
2018-12-08 12:20:50 -0500

    added javafx, andromda, still need gradle to point to Main Class

---
+++ src/intelliJ/src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String [] args){
+        Main m = new Main();
+        System.out.println("Test print from Main method.");
+        m.go();
+    }
+
+    public void go(){
+        System.out.println("Test print from Main.go()");
+
+    }
+}
