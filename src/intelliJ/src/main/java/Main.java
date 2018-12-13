2bca1702bc8c9cdc3f23fd0ce86090338ceb490b
skiwheelr <skiwheelr@gmail.com>
2018-12-13 02:44:18 -0500

    ready for alt-route if pop<execute> can be converted to gradle

--- src/intelliJ/src/main/java/Main.java
+++ src/intelliJ/src/main/java/Main.java
@@ -1,5 +1,9 @@
 import javafx.animation.*;
-
+import static def.dom.Globals.alert;
+import static def.jquery.Globals.$;
+import java.util.ArrayList;
+import java.util.List;
+import def.js.Array.push;
 
 public class Main {
 
@@ -7,6 +11,19 @@ public class Main {
         Main m = new Main();
         System.out.println("Test print from Main method.");
         m.go();
+
+        /*// you can use regular Java API
+        List<String> l = new ArrayList<>();
+        l.add("Hello");
+        l.add("world");
+        //// and you can also use regular JavaScript APIs
+        Array<String> a = new Array<>();
+        a.push("Hello", "world");
+        // use of jQuery with the jQuery candy
+        $("#target").text(l.toString());
+        // use of the JavaScript DOM API
+        alert(a.toString());*/
+
     }
 
     public void go(){
